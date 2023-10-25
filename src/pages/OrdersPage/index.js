03c06ff98b516539cae6362e71c8886a2821016f
palakkeni5 <palakkeni@gmail.com>
2023-10-25 16:31:47 -0400

    added navigation to the project. Created Cart and Orders pages

---
+++ src/pages/OrdersPage/index.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Box from "@mui/material/Box";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { productData } from "../../utils/product-data-json";
+import { DataGrid } from "@mui/x-data-grid";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+
+const OrdersPage = () => {
+  return (
+    <>
+      <Container component="main" sx={{ pt: 2 }}>
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h2" sx={{ mt: 10 }}>
+            Orders Page
+          </Typography>
+          <Typography variant="h6" sx={{ mb: 3 }}>
+            Your previous orders are below
+          </Typography>
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default OrdersPage;
