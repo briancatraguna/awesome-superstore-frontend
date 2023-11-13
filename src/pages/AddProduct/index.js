404ac552cc43515402a379709d138a802608e8b9
Brian Catraguna <brian.catraguna@gc.com>
2023-11-13 17:11:41 -0500

    Add entry point

--- src/pages/AddProduct/index.js
+++ src/pages/AddProduct/index.js
@@ -38,10 +38,7 @@ const AddProduct = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+
   };
 
   return (
@@ -56,9 +53,6 @@ const AddProduct = () => {
             alignItems: "center",
           }}
         >
-          {/* <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <PersonAddIcon />
-          </Avatar> */}
           <Typography component="h1" variant="h5">
             Add New Product
           </Typography>
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -18,6 +18,8 @@ import {
   emitNotification,
 } from "../../utils/emitNotification";
 import {getAllProducts} from "../../api/apiService";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
@@ -109,6 +111,9 @@ const Dashboard = () => {
           <Typography variant="h6" sx={{ mb: 3 }}>
             Select products below to add to cart
           </Typography>
+          <Button variant="contained" sx={{mb:3}} startIcon={<AddIcon/>} onClick={e => navigate(ROUTE_PATHS.addProduct)}>
+            Add new product
+          </Button>
 
           <DataGrid
             sx={{ mb: 10 }}
